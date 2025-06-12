17d1af28bb9c6b7d34d426aee1de2470cbb94dab
DyuthiHareesh <24dyuthi@gmail.com>
2025-06-12 15:12:25 +0530

    added maximum depth solution

---
+++ dsa/arrays/maximum-nesting-depth/maximumnestingdepth.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+       int currentdepth=0;
+       int maxdepth=0;
+       for(char ch:s.toCharArray()){
+        if(ch=='('){
+            currentdepth++;
+            maxdepth=Math.max(maxdepth,currentdepth);
+        }
+        else if(ch==')'){
+            currentdepth--;
+        }
+       } 
+       return maxdepth;
+    }
+}
