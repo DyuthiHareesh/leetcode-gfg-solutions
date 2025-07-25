f96d4c2342b43074d36afa387bbe4cc40c0263df
DyuthiHareesh <147138320+DyuthiHareesh@users.noreply.github.com>
2025-07-25 23:21:06 +0530

    Update mininrotated.java

--- dsa/searching-sorting/min-in-rotated-array/mininrotated.java
+++ dsa/searching-sorting/min-in-rotated-array/mininrotated.java
@@ -5,15 +5,15 @@ class Solution {
 
         while(low<=high){
             int mid=low+(high-low)/2;
-
+           //Since the array is rotated, if mid > high, then mid must be part of the left side, and the minimum lies to the right.
             if(nums[mid]>nums[high]){
                 low=mid+1;
             }
-
+//The right half is sorted, so the minimum must be on the left, possibly at mid.
             else if(nums[mid]<nums[high]){
                 high=mid;
             }
-
+        //nums[mid] == nums[high]
             else{
                 high--;
             }
